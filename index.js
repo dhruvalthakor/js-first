2b61aa2ad1d76932433f91b42768c8e85a7241dc
dhruvalthakor <dhruvalthakor007@gmail.com>
2024-04-10 10:46:50 +0530

    10-4-24

---
+++ index.js
@@ -0,0 +1,43 @@
+//--- Q-1---
+
+// let c=+prompt("enter a celsius")
+
+// let f=c*9/5+32;
+
+// console.log("the temperature in fahrenheit=",f);
+
+
+
+
+// ------Q2-----
+
+let base=+prompt("enter your base salary")
+
+let HRA=+prompt("enter  HRA")
+
+let DA=+prompt("enter DA")
+
+let TA=+prompt("enter TA")
+
+
+ HRA= base*HRA/100;
+
+ DA= base*DA/100;
+
+ TA=base*TA/100;
+
+ let gross=base+(HRA*base/100)+(DA*base/100)+(TA*base)/100;
+console.log("GrossSalary is = ",gross);
+
+
+
+
+
+// ------Q3-----
+
+// let first=+prompt("first targle")
+// let second=+prompt("second targle")
+
+// let A=first+second;
+// let third=180-A;
+// console.log("third angle=",third);
